d20cbd68339a76cda27d281163b2db03b753a9e9
tawseef <tawseefalam1@gmail.com>
2024-09-27 23:34:00 +0530

    http-status package added and implemented

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,10 +1,12 @@
+const httpStatus = require("http-status");
 const { upload, balance } = require("../service/service");
+
 
 // File Upload Function
 const handleFileUpload = async (req, res) => {
   const response = await upload(req.body);
-  if (response) res.status(200).json(response);
-  else res.status(400).json({ msg: "error" });
+  if (response) res.status(httpStatus.OK).json(response);
+  else res.status(httpStatus.BAD_REQUEST).json({ msg: "Internal server error" });
 };
 
 // Get Balance Function
@@ -12,8 +14,8 @@ const getBalance = async (req, res) => {
   const { timestamp } = req.body;
 
   const response = await balance(timestamp);
-  if (response) res.status(200).json(response);
-  else res.status(400).json({ msg: "error" });
+  if (response) res.status(httpStatus.OK).json(response);
+  else res.status(httpStatus.BAD_REQUEST).json({ msg: "Internal server error" });
 };
 
 module.exports = { handleFileUpload, getBalance };
